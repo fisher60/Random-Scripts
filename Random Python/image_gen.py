f6abfc74894423c82c340f088a6275c47a0ad3d2
fisher60 <wakefollower@gmail.com>
2020-02-26 18:30:34 -0800

    added git ignore, minor image tweaks

--- Random Python/image_gen.py
+++ Random Python/image_gen.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os
 
 from PIL import Image
+
 
 def create_image():
     img = Image.new('RGB', [800,800], 255)
@@ -11,22 +13,14 @@ def create_image():
 
     for x in range(img.size[0]):
         for y in range(img.size[1]):
-            if draw_square(x, y, 300, 100):
-                data[x,y] = (
-                    x % 255,
-                    y % 255,
-                    (x + y) % 255
-                )
-            else:
-                data[x,y] = draw_curve_repeat(x, y, img, bg_color)
-    path = 'images/'
-    name = 'image.png'
-    img.save(path + name)
-    display_image(path + name)
+            data[x,y] = draw_curve_repeat(x, y, img, [0, 255, 50])
+    save_image(img)
+
 
 def is_line(grid_value: int, line_value, pixel_width=5):
     line_val_true = line_value + pixel_width > grid_value > line_value - pixel_width
     return line_val_true
+
 
 def draw_square(x, y, side_length: int, pixel_width=5):
     x_true = is_line(x, side_length, pixel_width) or is_line(x, side_length * 2, pixel_width)
@@ -35,6 +29,7 @@ def draw_square(x, y, side_length: int, pixel_width=5):
     y_position_true = side_length + pixel_width/4 > y - side_length > 0
     return (x_true or y_true) and y_position_true and x_position_true
 
+
 def draw_curve_repeat(x, y, img, color: list):
     for count, each in enumerate(color):
         if each < 1:
@@ -42,9 +37,21 @@ def draw_curve_repeat(x, y, img, color: list):
     curve_options = (((x * y) // img.size[0]) % color[0], ((x * y) // img.size[0]) % color[1], ((x * y) // img.size[0]) % color[2])
     return curve_options
 
+
 def display_image(filename):
     img=mpimg.imread(filename)
     imgplot = plt.imshow(img)
     plt.show()
+
+
+def save_image(image):
+    path = 'images/'
+    i = 0
+    while os.path.exists(path + 'image%s.png' % i):
+        i += 1
+    name = f'image{i}.png'
+    image.save(path + name)
+    display_image(path + name)
+
 
 create_image()
