4aa5d6bd034b4f0ea43c06f0d5c53e098e8e099e
fisher60 <wakefollower@gmail.com>
2020-11-25 14:32:39 -0800

    update commit

--- Random Python/test.py
+++ Random Python/test.py
@@ -1,23 +1,19 @@
-def shorten_path(path):
-    temp_count = 0
-    index_offset = 0
-    new_list = []
-
-    for index in range(len(path)):
-        if index + index_offset >= len(path):
-            break
-
-        item = path[index + index_offset]
-        print(item)
-        temp_count += 1
-        while index + index_offset < len(path) and path[index + index_offset] == item:
-            # print(path[index + index_offset])
-            temp_count += 1
-            index_offset += 1
-        new_list.append(f"{path[index]} {temp_count}")
-        temp_count = 0
-
-    return new_list
+import random
 
 
-print(shorten_path(["right", "right", "left", "down", "left", "up"]))
+def run_match():
+    sum = 0
+
+    for _ in range(7):
+        print("game")
+        if random.randint(0, 100) < 60:
+            sum += 1
+            if sum == 4:
+                print("win")
+                break
+    else:
+        print("LOSE")
+
+
+for match in range(10**5):
+    run_match()
